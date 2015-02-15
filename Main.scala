26c85d21323c0946e443fe0563490a32c576e471
Roman Franchuk <franchukrom@gmail.com>
2015-02-15 15:44:30 +0200

    Refactor demos

--- Main.scala
+++ Main.scala
@@ -6,28 +6,33 @@ import MathTools._
 
 object Main {
   def squareDemo {
-    val scene = new Scene(1920, 1080, 2 * Pi / 3)
-    val t1 = Triangle(Vector3(-2, -2, 3), Vector3(-2, 2, 3), Vector3(2, -2, 3), Material(intToColor(0xcccccc), intToColor(0xffffff), 100))
-    val t2 = Triangle(Vector3(2, -2, 3), Vector3(-2, 2, 3), Vector3(2, 2, 3), Material(intToColor(0xcccccc), intToColor(0xffffff), 100))
-    scene.addShape(t1)
-    scene.addShape(t2)
-
-    scene.addLight(0, 0, 0, 0xffffff)
-
-    val img = new BufferedImage(1920, 1080, BufferedImage.TYPE_INT_RGB)
-    scene.render(img.setRGB _)
-
-    ImageIO.write(img, "PNG", new File("result.png"))
+    render(
+      List(
+        Triangle(Vector3(-2, -2, 3), Vector3(-2, 2, 3), Vector3(2, -2, 3), Material(intToColor(0xcccccc), intToColor(0xffffff), 100)),
+        Triangle(Vector3(2, -2, 3), Vector3(-2, 2, 3), Vector3(2, 2, 3), Material(intToColor(0xcccccc), intToColor(0xffffff), 100))
+      ),
+      List(Light(Vector3(0, 0, 0), intToColor(0xffffff))),
+      Vector3(0, 0, 0))
   }
 
   def sphereDemo {
+    render(
+      List(
+        Sphere(1, 1, 4.5, 0.5, 0xffff00, 0xffffff, 20),
+        Sphere(0, -1, 5, 1.5, 0xffff00, 0xffffff, 20)
+      ),
+      List(Light(Vector3(1,5,5), intToColor(0xffffff))),
+      Vector3(0.2, 0.2, 0.2)
+    )
+  }
+
+  def render(shapes: List[Shape], lights: List[Light], ambient: Vector3) {
     val scene = new Scene(1920, 1080, 2 * Pi / 3)
 
-    scene.addShape(Sphere(1, 1, 4.5, 0.5, 0xffff00, 0xffffff, 20))
-    scene.addShape(Sphere(0, -1, 5, 1.5, 0xffff00, 0xffffff, 20))
+    scene.shapes = shapes
+    scene.lights = lights
 
-    scene.ambientLight = Vector3(0.2, 0.2, 0.2)
-    scene.addLight(1, 5, 5, 0xffffff)
+    scene.ambientLight = ambient
 
     val img = new BufferedImage(1920, 1080, BufferedImage.TYPE_INT_RGB)
     scene.render(img.setRGB _)

64f82abb1876617786e2b15cfc2e48fadad329a8
Roman Franchuk <franchukrom@gmail.com>
2015-02-15 02:29:59 +0200

    Simplify demo program

--- Main.scala
+++ Main.scala
@@ -6,13 +6,9 @@ import scala.math._
 object Main {
   def main(args: Array[String]) {
     val scene = new Scene(640, 480, 2 * Pi / 3)
-    scene.addSphere(0, 5, 5, 1, 0xffffff)
     scene.addSphere(0, 0, 1, 0.5, 0xee1111)
-
     scene.ambientLight = Vector3(0.1, 0.1, 0.1)
-
-    scene.addLight(5, 0, 0, 0xffff00)
-    scene.addLight(-5, 0, 0, 0x0000ff)
+    scene.addLight(5, 0, 0, 0xffffff)
 
     val img = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB)
     scene.render(img.setRGB _)

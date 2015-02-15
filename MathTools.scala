51c57ac09ca33df851314a5839ba4bfcc1c1e79a
Roman Franchuk <franchukrom@gmail.com>
2015-02-15 03:47:05 +0200

    Get rid of the unused obsolete function

--- MathTools.scala
+++ MathTools.scala
@@ -1,20 +1,6 @@
 import scala.math._
 
 object MathTools {
-  def applyIntensityToColor(color: Int, intensity: Double) = {
-    if (intensity >= 1)
-      color
-    else if (intensity < 0)
-      0
-    else {
-      //Deconstruct the color
-      val (red, green, blue) = ((color & 0xff0000) >> 16, (color & 0xff00) >> 8, color & 0xff)
-      // Multiply each component by intensivity
-      val (new_red, new_green, new_blue) = ((red * intensity).toInt, (green * intensity).toInt, (blue * intensity).toInt)
-      (new_red << 16) | (new_green << 8) | new_blue
-    }
-  }
-
   def colorToInt(color: Vector3) = {
     val color255 = color * 255
     (color255.x.toInt << 16) | (color255.y.toInt << 8) | color255.z.toInt
